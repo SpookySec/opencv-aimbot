57247a4abcfc25434ec23e804756ed4e07362b75
Cloasies <cloasies@gmail.com>
2021-07-15 13:57:00 +0300

    Added log + apply_threshold

--- utils.py
+++ utils.py
@@ -7,6 +7,21 @@ def display(image):
     cv.imshow('reader', image)
     cv.waitKey(0)
 
+def log(value):
+
+    print(f"""
+     
+     ================
+     VALUE: {value}
+     =================
+     """)
+     
+def apply_threshold(val, confidence):
+
+    if val > confidence:
+        return True
+    return False
+
 def move_to(x, y):
     win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, x, y, 0, 0)
 
@@ -14,4 +29,4 @@ def resize_img(img, scale):
     width = int(img.shape[1] * scale / 100)
     height = int(img.shape[0] * scale / 100)
     dim = (width, height)
-    return cv.resize(img, dim)
+    return cv.resize(img, dim);
