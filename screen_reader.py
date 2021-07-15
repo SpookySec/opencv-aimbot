23b8a4c639c4738958a3679078b576fafdf43d9a
SpookySec <anonymousbot100110@gmail.com>
2021-07-15 14:52:51 +0300

    code cleaned

--- screen_reader.py
+++ screen_reader.py
@@ -4,26 +4,35 @@ import utils
 import numpy as np
 import pyautogui as ag
 import win32api, win32con
+from time import sleep
 
 needle = cv.imread('./images/needle3.png', cv.IMREAD_GRAYSCALE)
-with mss.mss() as sct:
-    monitor = {"top": 0, "left": 0, "width": 1920, "height": 1080}
-    while True:
-        screenshot = np.array(sct.grab(monitor))
+monitor = {"top": 0, "left": 0, "width": 1920, "height": 1080}
+sct = mss.mss()
 
-        haystack = np.array(sct.grab(monitor))
-        # haystack = cv.imread('./images/haystack.png')
+while True:
 
-        haystack = cv.cvtColor(haystack, cv.COLOR_BGR2GRAY)
-        result = cv.matchTemplate(haystack, needle, cv.TM_CCOEFF_NORMED)
-        min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
-        utils.move_to(int(max_loc[0] - 960), int(max_loc[1] - 540))
-        found = cv.circle(screenshot, (max_loc[0] + 11, max_loc[1] + 13), 17, (0, 0, 255), 2)
-        scaled_found = utils.resize_img(found, 70)
-        scaled_result = utils.resize_img(result, 70)
+    # Take a screen shot to draw on later
+    screenshot = np.array(sct.grab(monitor))
 
-        cv.imshow("Best matches", scaled_found)
-            # Press "q" to quit
-        if cv.waitKey(30) & 0xFF == ord("q"):
-            cv.destroyAllWindows()
-            break
+    # Current screen (GRAY)
+    haystack = cv.cvtColor(np.array(sct.grab(monitor)), cv.COLOR_BGR2GRAY)
+
+    # Results from template matching
+    result = cv.matchTemplate(haystack, needle, cv.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+
+    # Finally drawing a circle
+    if max_val > .8:
+        circle = cv.circle(screenshot, (max_loc[0] + 11, max_loc[1] + 13), 17, (0, 0, 255), 2)
+        line = cv.line(circle, (960, 540), max_loc, (255, 255, 255), 2)
+        cv.imshow("Best matches", line)
+    else:
+        cv.imshow("Best matches", screenshot)
+
+    # utils.move_to(int(max_loc[0] - 960), int(max_loc[1] - 540))
+
+        # Press "q" to quit
+    if cv.waitKey(30) & 0xFF == ord("q"):
+        cv.destroyAllWindows()
+        break
