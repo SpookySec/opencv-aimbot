0ea030ad5ecefdcaf46754278ee35282a65088be
SpookySec <anonymousbot100110@gmail.com>
2021-07-16 13:02:32 +0300

    the bot shoots now lmao

--- screen_reader.py
+++ screen_reader.py
@@ -9,10 +9,10 @@ from time import sleep
 needle = cv.imread('./images/needle3.png', cv.IMREAD_GRAYSCALE)
 monitor = {"top": 0, "left": 0, "width": 1920, "height": 1080}
 sct = mss.mss()
-threshold = 0.75
+threshold = 0.65
 scale = 0.7
 w, h = 50, 60
-x_off, y_off = 20, 40
+x_off, y_off = 20, 50
 screen_center = (960, 540)
 
 while True:
@@ -39,27 +39,28 @@ while True:
 
     if len(heads) < 1:
         print("[!] No heads found :(")
-        exit(1)
 
-    print(f"\r[+] Found {len(heads)} heads\n", end="")
+    else:
+        print(f"\r[+] Found {len(heads)} heads\n", end="")
 
-    closest = heads[0]
+        closest = heads[0]
 
-    entity_center = (closest[0] + w // 2, closest[1] + h // 2)
+        entity_center = (closest[0] + w // 2, closest[1] + h // 2)
 
-    print(entity_center)
-    rect = cv.rectangle(screenshot, (closest[0], closest[1]), (closest[0] + w, closest[1] + h), (0, 255, 0), 2)
-    line = cv.line(rect, screen_center, entity_center, (255, 255, 255), 2)
+        print(entity_center)
+        rect = cv.rectangle(screenshot, (closest[0], closest[1]), (closest[0] + w, closest[1] + h), (0, 255, 0), 2)
+        line = cv.line(rect, screen_center, entity_center, (255, 255, 255), 2)
 
 
-    # cv.imshow("Best matches", utils.resize_img(line, 75))
+        # cv.imshow("Best matches", utils.resize_img(line, 75))
 
-    # utils.move_to(x, y)
+        # utils.move_to(x, y)
 
-    win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, int((entity_center[0] - screen_center[0]) * scale), int((entity_center[1] - screen_center[1]) * scale), 0, 0 )
-    # utils.move_to(int(max_loc[0] - 960), int(max_loc[1] - 540))
-
+        win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, int((entity_center[0] - screen_center[0]) * scale), int((entity_center[1] - screen_center[1]) * scale), 0, 0 )
+        shoot_x, shoot_y = int((entity_center[0] - screen_center[0]) * scale), int((entity_center[1] - screen_center[1]) * scale)
+        utils.shoot(shoot_x, shoot_y)
+    sleep(1)
         # Press "q" to quit
-    if cv.waitKey(1) & 0xFF == ord("q"):
-        cv.destroyAllWindows()
-        break
+    # if cv.waitKey(1) & 0xFF == ord("q"):
+    #     cv.destroyAllWindows()
+    #     break
--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import cv2 as cv
 import numpy as np
 import pyautogui as pag
 import win32api, win32con
+import time
 
 def display(image):
     cv.imshow('reader', image)
@@ -32,3 +33,9 @@ def distance_squared(pt1, pt2):
     diffX = pt1[0] - pt2[0]
     diffY = pt1[1] - pt2[1]
     return (diffX*diffX+diffY*diffY);
+
+def shoot(x, y):
+    time.sleep(0.05)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
+    time.sleep(0.1)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
